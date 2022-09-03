1bf4c21c6d978ebc55222149458e56736a5f803e
Gjuni <kmj06178@gmail.com>
2022-09-03 17:36:01 +0900

    Create 1주차 printf().c
    encoding to korean Fail

---
+++ C프 강의 실습 VS Code/C프 강의 실습/1주차 printf().c
@@ -0,0 +1,18 @@
+/* C programming first week practice exercise
+	name : your name
+	Student ID : your student ID
+	You must use %d for Student ID */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Name : %s\n", "JUNE");
+	printf("Student ID : %d\n", 202010836);
+
+    //github utf-8 encoding Test in VS code 
+
+    printf("이름 : %s\n", "고명준");
+	printf("학번 : %d", 202010836);
+	return 0;
+}
